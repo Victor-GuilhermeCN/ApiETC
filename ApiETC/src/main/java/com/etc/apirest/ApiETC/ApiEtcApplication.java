d08fc9f52675809b93c944d979fbcf71165edc18
Victor <victor258369@hotmail.com>
2020-10-29 12:05:36 -0300

    Base do projeto - Iniciado no spring initializer

---
+++ ApiETC/src/main/java/com/etc/apirest/ApiETC/ApiEtcApplication.java
@@ -0,0 +1,13 @@
+package com.etc.apirest.ApiETC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEtcApplication.class, args);
+	}
+
+}
---
+++ ApiETC/src/test/java/com/etc/apirest/ApiETC/ApiEtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etc.apirest.ApiETC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
